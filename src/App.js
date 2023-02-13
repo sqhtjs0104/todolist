ba910432f58276c77563ed74d2012c1562236f8f
sqhtjs0104 <sqhtjs0104@gmail.com>
2023-02-13 18:02:16 +0900

    Add CSS
    CSS 추가 및 애니메이션 처리

--- src/App.js
+++ src/App.js
@@ -3,32 +3,114 @@ import styled from 'styled-components';
 
 import Todo from './components/Todo';
 
+const Main = styled.div`
+  width: 100%;
+  height: 100%;
+
+  padding: 10px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-flow: column nowrap;
+
+  .title {
+    font-size: 30px;
+    margin: auto;
+  }
+
+  .adding {
+    width: 100%;
+    padding: 10px;
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+
+    * {
+      border-radius: 5px;
+      font-size: 14px;
+      padding: 8px;
+    }
+
+    input {
+      margin-right: 10px;
+      border: 2px solid #000;
+    }
+
+    button {
+      border: none;
+      background-color: #5a5ad4;
+      color: white;
+      padding: 8px 10px;
+      font-weight: 600;
+
+      &:hover {
+        cursor: pointer;
+        scale: 1.1;
+      }
+
+      &:active {
+        scale: 1;
+        background-color: #313186;
+      }
+    }
+  }
+  
+  fieldset {
+    border: 2px solid #555;
+    border-radius: 10px;
+    margin-top: 10px;
+
+    legend {
+      font-size: 16px;
+      padding: 0px 5px;
+      color: #333;
+    }
+
+    ul {
+      padding: 10px;
+      margin: 0;
+      list-style: none;
+
+      li {
+        margin-bottom: 5px;
+      }
+    }
+  }
+
+  .processing {
+  }
+
+  .completed {
+  }
+`
+
 const App = memo(() => {
   return (
-    <>
-      <h1>To Do List</h1>
-      <div> {/** 상단 제어 영역 */}
-        <form>
-          <input type="text" />
-          <button type='submit'>Add</button>
-        </form>
+    <Main>
+      <h1 className='title'>To Do List</h1>
+      <div className='adding'> {/** 상단 제어 영역 */}
+        <input type="text" />
+        <button>Add</button>
       </div>
 
       <div>
-        <fieldset>
-          <legend>To Do</legend>
+        <fieldset className='processing'>
+          <legend>Processing</legend>
           <ul> {/** 중단 현재 Todo list 영역 */}
-            <Todo />
+            <Todo number={1} />
+            <Todo number={2} />
+            <Todo number={3} />
           </ul>
         </fieldset>
-        <fieldset>
+        <fieldset className='completed'>
           <legend>Completed</legend>
           <ul> {/** 하단 완료한 Todo list 영역 */}
-            <Todo />
+            <Todo number={4} />
+            <Todo number={5} />
           </ul>
         </fieldset>
       </div>
-    </>
+    </Main>
   );
 });
 
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,12 +1,57 @@
-import React, { memo } from 'react';
+import React, { memo, useCallback } from 'react';
+import styled from 'styled-components';
 
-const Todo = memo(() => {
+const TodoItem = styled.li`
+	display: flex;
+	flex-flow: row nowrap;
+	align-items: center;
+
+	input[type="checkbox"] {
+		display: none;
+	}
+
+	label {
+		display: inline-block;
+		width: 20px;
+		height: 20px;
+		border: 1px solid #000;
+		border-radius: 5px;
+		background-color: #fff;
+		color: white;
+		transition: all 0.5s;
+		box-sizing: border-box;
+		margin-right: 10px;
+		text-align: center;
+
+		&:hover {
+			cursor: pointer;
+		}
+	}
+
+	input[type="checkbox"]:checked + label {
+		background-color: #5a5ada;
+	}
+
+	span {
+		margin-right: 5px;
+		font-size: 20px;
+		color: green;
+		transition: all 0.5s;
+	}
+
+	input[type="checkbox"]:checked + label + span {
+		text-decoration: line-through;
+		color: #333;
+	}
+`
+
+const Todo = memo(props => {
 	return (
-		<li>
-			<input type="checkbox" />
+		<TodoItem>
+			<input id={`todoCheck${props.number}`} type="checkbox" />
+			<label htmlFor={`todoCheck${props.number}`}>V</label>
 			<span>Hello</span>
-			<button>Modify</button>
-		</li>
+		</TodoItem>
 	);
 });
 
