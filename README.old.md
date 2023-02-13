516a1be3b2a1c54f35bda4a5216c941125e3f6b6
sqhtjs0104 <sqhtjs0104@gmail.com>
2023-02-13 16:34:36 +0900

    Create new React App

--- README.old.md
+++ README.old.md
@@ -1,4 +1,2 @@
 # todolist
 Side project - To do list
-
-> Created at 2023-02-13 16:30
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { memo } from 'react';
+
+const App = memo(() => {
+  return (
+    <>
+      
+    </>
+  );
+});
+
+export default App;
